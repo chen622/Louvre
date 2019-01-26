29746ae47beae778ee7a4c9e76a5ae2c60df5610
lucky98622 <745478424@qq.com>
2019-01-26 22:47:46 +0800

    111

--- main.py
+++ main.py
@@ -7,51 +7,41 @@ from model.Floor import Floor
 from model.Item import Item
 from model.Stair import Stair
 
-FLOORS = 2
-ROWS = 4
-COLUMNS = 6
+FLOORS = 5
+# FLOORS = 2
+ROWS = 58 * 5
+# ROWS = 2
+COLUMNS = 136 * 5
+# COLUMNS = 3
 exit_floor = []
-exits = []  # The position of exit
+exits = [[]]  # The position of exit
 stairs = []
 louvre_map = numpy.empty([FLOORS, ROWS, COLUMNS], dtype=Item)
 connect_floor = []
 
 
 def read_data():
-    excel = xlrd.open_workbook(r'./data.xlsx')
+    excel = xlrd.open_workbook(r'./data2.xlsx')
     for f in range(FLOORS):
         sheet = excel.sheet_by_index(f)
         for i in range(ROWS):
             for j in range(COLUMNS):
-                value = int(sheet.cell(i, j).value)
+                value = int(sheet.cell(int(i / 5), int(j / 5)).value)
                 if value == 1:
                     louvre_map[f][i][j] = Floor()
-                elif value == 2:
+                elif value == 3:
                     stairs[f].append((i, j))
                     louvre_map[f][i][j] = Stair(0)
-                elif value == 3:
+                elif value == 4:
                     exit_floor.append(f)
                     exits[f].append((i, j))
                     louvre_map[f][i][j] = Exit()
-                elif value == 4:
+                elif value == 2:
                     stairs[f].append((i, j))
                     louvre_map[f][i][j] = Stair(1)
                 else:
                     louvre_map[f][i][j] = Blank()
     count_heuristic()
-    for floor in louvre_map:
-        for row in floor:
-            for value in row:
-                if isinstance(value, Floor):
-                    print('\033[0;37;44m  \033[0m', end='')
-                elif isinstance(value, Stair):
-                    print('\033[0;37;46m %.2f\033[0m' % value.heuristic, end='')
-                elif isinstance(value, Exit):
-                    print('\033[0;37;42m  \033[0m', end='')
-                else:
-                    print('\033[0;37;47m  \033[0m', end='')
-            print()
-        print(end='\n\n\n')
 
 
 def count_heuristic():
@@ -94,6 +84,22 @@ def count_all_floor_stair():
                 louvre_map[f + 1][x_stair][y_stair].is_up_to_down(louvre_map[f][x_stair][y_stair].heuristic + 15)
                 if f + 1 not in connect_floor: connect_floor.append(f + 1)
                 exits[f + 1].append((x_stair, y_stair))
+
+
+def printGraph():
+    for floor in louvre_map:
+        for row in floor:
+            for value in row:
+                if isinstance(value, Floor):
+                    print('\033[0;37;44m  \033[0m', end='')
+                elif isinstance(value, Stair):
+                    print('\033[0;37;46m %.2f\033[0m' % value.heuristic, end='')
+                elif isinstance(value, Exit):
+                    print('\033[0;37;42m  \033[0m', end='')
+                else:
+                    print('\033[0;37;47m  \033[0m', end='')
+            print()
+        print(end='\n\n\n')
 
 
 if __name__ == '__main__':
